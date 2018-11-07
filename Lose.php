7af804cf17226ec69e49d22b7883ef580808805a
David Encina Martínez <36929101+Dencina1996@users.noreply.github.com>
2018-11-07 20:26:51 +0100

    Add files via upload

---
+++ Lose.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/png" href="Images/Lose.png">
+	<title>PERDEDOR!</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="scripts.js"></script>
+</head>
+<body id="bodyLose" onload="LlamarLoser()">
+	<h1>¡¡¡PERDEDOR!!!</h1>
+</body>
+</html>
